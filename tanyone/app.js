1b9e74df04a08ff46516e7e73f0950d8d6a58835
kerimskill <71585229+kerimskill@users.noreply.github.com>
2020-09-20 14:25:37 +0600

    Сайт не законченный

---
+++ tanyone/app.js
@@ -0,0 +1,10 @@
+
+let name;
+let age;
+name = "jone";
+age = 26;
+
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".navLinks");
+
+console.log(navLinks);
